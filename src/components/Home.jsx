341095bd89aaf13e6315675e5aeb93d06fa04b68
Chirag Pandey <pndy.chrg2003@gmail.com>
2023-12-31 15:14:43 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+import Send from "./Send";
+import Receive from "./Receive";
+
+
+const Home = () => {
+
+    const [isSendActive, setIsSendActive] = useState(true);
+
+    return (
+        <div className="flex flex-col items-center justify-center mt-5 p-5">
+            <h1 className="md:text-9xl text-center text-7xl">Wanna Share a Image ?</h1>
+            <div className="flex flex-row justify-around items-center mt-10 w-full flex-wrap">
+                <button className="border-2 rounded-lg px-6 py-3 text-5xl shadow-lg" onClick={() => (setIsSendActive(true))}>Send</button>
+                <button className="border-2 rounded-lg px-6 py-3 text-5xl shadow-lg" onClick={() => (setIsSendActive(false))}>Recieve</button>
+            </div>
+            <div className=" flex justify-center items-center mt-10 border-2 rounded-lg p-5 ">
+                {isSendActive ? <Send /> : <Receive />}
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom"
+
+const Navbar = () => {
+    return (
+        <nav className="fixed top-0 z-30 mx-auto w-full max-w-screen-sm border border-gray-100 bg-white/80 py-3 shadow backdrop-blur-lg md:top-4 md:rounded-3xl lg:max-w-screen-lg inset-x-0">
+            <div className="px-4">
+                <div className="flex items-center justify-between">
+                    <div>
+                        <Link href="#" className="">Share It</Link>
+                    </div>
+                    <div >
+                        {/* Add download and upload buttons here which scrolls the page to them */}
+                        <Link href="#" className="me-2" >Download</Link>
+                        <Link href="#" >Upload</Link>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
